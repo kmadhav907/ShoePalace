3353256860682f5096d2c017bedd13e8f0324c86
Madhav Bhat K <mbhatk@BLR.ALLEGISINDIA.COM>
2022-05-10 00:45:27 +0530

    Basic Login page

--- src/main/java/com/shoeshop/application/controllers/UserController.java
+++ src/main/java/com/shoeshop/application/controllers/UserController.java
@@ -3,16 +3,35 @@ package com.shoeshop.application.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.shoeshop.application.entity.User;
 
 @Controller
 public class UserController {
 	@GetMapping("/")
-	public String getLoginPage() {
-		return "index";
-		
+	public String getHomePage() {
+		return "index";	
 	}
+	
+	@GetMapping("/login")
+	public String getLoginPage(User user) {
+		return "login";
+	}
+	
+	@PostMapping("/login")
+	public RedirectView postLoginPage(User user) {
+		System.out.println(user.getEmail());
+		return new RedirectView("/dashboard");
+	}
+	@GetMapping("/dashboard")
+	public String viewDashBoard() {
+		return "dashboard";
+	}
+	
 	
 
 }
